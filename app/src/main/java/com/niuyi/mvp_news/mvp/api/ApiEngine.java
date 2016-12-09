9b8972abc74aec2e262ed42ad5fbfd58064b8d7e
niuyi <764537773@qq.com>
2016-12-09 17:42:34 +0800

    缓存配置

--- app/src/main/java/com/niuyi/mvp_news/mvp/api/ApiEngine.java
+++ app/src/main/java/com/niuyi/mvp_news/mvp/api/ApiEngine.java
@@ -2,12 +2,18 @@ package com.niuyi.mvp_news.mvp.api;
 
 import com.niuyi.mvp_news.application.MyApplication;
 import com.niuyi.mvp_news.constant.Constant;
+import com.niuyi.mvp_news.utils.NetworkUtil;
+import com.orhanobut.logger.Logger;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -21,6 +27,11 @@ public class ApiEngine {
     private volatile static ApiEngine apiEngine;
     private String mBaseUrl;
     private Retrofit retrofit;
+
+    // 设缓存有效期为两天
+    private static final long CACHE_STALE_SEC = 60 * 60 * 24 * 2;
+    // 30秒内直接读缓存
+    private static final long CACHE_AGE_SEC = 0;
 
     private ApiEngine(int type) {//type用来区分url
         //日志拦截器
@@ -36,7 +47,8 @@ public class ApiEngine {
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .writeTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(10, TimeUnit.SECONDS)
-                .addNetworkInterceptor(new NetworkInterceptor())
+                .addNetworkInterceptor(mCacheControlInterceptor)
+                .addInterceptor(mCacheControlInterceptor)
                 .addInterceptor(loggingInterceptor)
                 .cache(cache)
                 .build();
@@ -76,4 +88,52 @@ public class ApiEngine {
     public ApiService getApiService() {
         return retrofit.create(ApiService.class);
     }
+
+    // 配置缓存
+    private Interceptor mCacheControlInterceptor = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            Logger.e("--------------------------------------------缓存----------------------------------------------------");
+            // 在这里统一配置请求头缓存策略以及响应头缓存策略
+            if (NetworkUtil.isNetworkConnected(MyApplication.getContext())) {
+                Logger.e("--------------------------------------------有网络----------------------------------------------------");
+                // 在有网的情况下CACHE_AGE_SEC秒内读缓存，大于CACHE_AGE_SEC秒后会重新请求数据
+                request = request
+                        .newBuilder()
+                        .removeHeader("Pragma")
+                        .removeHeader("Cache-Control")
+                        .header("Cache-Control", "public, max-age=" + CACHE_AGE_SEC)
+                        .build();
+
+                Response response = chain.proceed(request);
+
+                return response
+                        .newBuilder()
+                        .removeHeader("Pragma")
+                        .removeHeader("Cache-Control")
+                        .header("Cache-Control", "public, max-age=" + CACHE_AGE_SEC)
+                        .build();
+            } else {
+                Logger.e("--------------------------------------------无网络----------------------------------------------------");
+                // 无网情况下CACHE_STALE_SEC秒内读取缓存，大于CACHE_STALE_SEC秒缓存无效报504
+                request = request
+                        .newBuilder()
+                        .removeHeader("Pragma")
+                        .removeHeader("Cache-Control")
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + CACHE_STALE_SEC)
+                        .build();
+
+                Response response = chain.proceed(request);
+
+                return response
+                        .newBuilder()
+                        .removeHeader("Pragma")
+                        .removeHeader("Cache-Control")
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + CACHE_STALE_SEC)
+                        .build();
+            }
+
+        }
+    };
 }
